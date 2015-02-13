2256648241faac323fec0f5b8324fab445a0dad6
Cyril Thomas <cyril.thomas@optiflows.com>
2015-02-13 15:44:27 +0100

    Wrapping the socketio launch inside a custom command for Flask Script

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager, Server, Command
 
 from collabo import create_app
 from collabo import socketio
@@ -6,8 +6,17 @@ from collabo import socketio
 app = create_app()
 manager = Manager(app)
 
+
+class Run(Command):
+
+    def __init__(self, app):
+        self._app = app
+
+    def run(self):
+        socketio.run(self._app)
+
 manager.add_command("debug", Server(use_debugger=True, use_reloader=True))
+manager.add_command("run", Run(app))
 
 if __name__ == "__main__":
-    socketio.run(app)
-    # manager.run()
+    manager.run()
